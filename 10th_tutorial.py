3c0fe1fdf956ac92b744b90faedc8c987ed86cf7
kaheonkim <kdkam12@unist.ac.kr>
2024-04-03 11:44:11 -0400

    Update 10th tutorial

---
+++ 10th_tutorial.py
@@ -0,0 +1,35 @@
+# print(ord('a'))
+# print(ord('z'))
+
+
+# print(ord('A'))
+# print(ord('Z'))
+
+# print(ord(' '))
+# print(chr(100))
+# print('ABC'.lower())
+
+
+# string = 'Fer biyonde the see;\n'
+# string = string.lower()
+
+# for a in string:
+#     print(a)
+
+# a = [1,2,3]
+# print(a[::-1])
+
+# b = set()
+# b.add(1)
+# b.add(2)
+# b.add(1)
+# b.add(2)
+# b.add(3)
+
+# b = []
+# b.append(1)
+# b.append(2)
+# b.append(1)
+# b.append(2)
+# b.append(3)
+# b = set(bb)
--- 9th_tutorial/9th_tutorial.py
+++ 9th_tutorial/9th_tutorial.py
@@ -1,26 +1,40 @@
 # Open the input file for reading
 with open("input_tutorial1.txt", "r") as input_file:
     # Read each line from the input file
+    line1 = input_file.readline()
+    input_file.readline()
+    line_left = input_file.readlines()
+
+
+with open("input_tutorial1.txt", "r") as input_file:
     lines = input_file.readlines()
     
     
     
     
-string = '1,2,3\n'
+    
+    
+    
+    
+# string = '1,2,3\n'
 
-splited_string = string.strip().split(',')
+# splited_string = string.strip().split(',')
 
-splited_string_int = [int(x) for x in splited_string]
+# splited_string_int = [int(x) for x in splited_string]
+
+
+
+
+
 
 
 
 # Open the input file for reading
 with open("input_tutorial1.txt", "r") as input_file:
-    # Read each line from the input file
     lines = input_file.readlines()
     
-line = lines[0]
-splited_line = [int(i) for i in line.strip().split(";")]
+# line = lines[0]
+# splited_line = [int(i) for i in line.strip().split(";")]
 
 
 matrix1 = []
@@ -30,36 +44,61 @@ for line in lines:
     
     
     
-A = [[1,2,3],[4,5,6]]
-B = [[2,4,10],[11,8,7]]
+    
+    
+    
+    
+    
+# A = [[1,2,3],[4,5,6]]
+# B = [[2,4,10],[11,8,7]]
 
-sum_AB = [[A[i][j]+B[i][j]for j in range(len(A[0]))] for i in range(len(A))]
+# sum_AB = [[A[i][j]+B[i][j]for j in range(len(A[0]))] for i in range(len(A))]
+
+
+
+
+
+
+
 
 
 
 A = [[1,2,10,58], [1,3,10], [10,9,1,5,7,9,7],[3]]
-with open("output_mean_1.txt","w") as output_file:
-    for entry in A:
-        mean = sum(entry)/len(entry)
-        output_file.write(f"{mean}\n")
+# with open("output_mean_1.txt","w") as output_file:
+#     for entry in A:
+#         mean = sum(entry)/len(entry)
+#         output_file.write(f"{mean}\n")
         
 
 
-# Open the input file for reading
-with open("input_tutorial2.txt", "r") as input_file:
-    # Read each line from the input file
-    lines = input_file.readlines()
+# mean_list = []
+# for entry in A:
+#     mean = sum(entry)/len(entry)
+#     mean_list.append(mean)
+
+# with open("output_mean_1_.txt","w") as output_file:
+#     for mean in mean_list:
+#         output_file.write(f"{mean}\n")
+
+
+
+
+
+# # Open the input file for reading
+# with open("input_tutorial2.txt", "r") as input_file:
+#     # Read each line from the input file
+#     lines = input_file.readlines()
     
 
-matrix2 = []
-for line in lines:
-    splited = line.strip().split("|")
-    splited_line = [int(i) for i in splited]
-    matrix2.append(splited_line)
+# matrix2 = []
+# for line in lines:
+#     splited = line.strip().split("|")
+#     splited_line = [int(i) for i in splited]
+#     matrix2.append(splited_line)
 
 
 
-with open("output_mean_2.txt","w") as output_file:
-    for entry in matrix2:
-        mean = sum(entry)/len(entry)
-        output_file.write(f"{mean}\n")
+# with open("output_mean_2.txt","w") as output_file:
+#     for entry in matrix2:
+#         mean = sum(entry)/len(entry)
+#         output_file.write(f"{mean}\n")
