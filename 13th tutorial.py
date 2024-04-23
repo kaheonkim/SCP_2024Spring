1ceabd01957c0eda65822e12bed1adfc80eeee7a
kaheonkim <kdkam12@unist.ac.kr>
2024-04-23 15:07:10 -0400

    Create 13th tutorial.py

---
+++ 13th tutorial.py
@@ -0,0 +1,39 @@
+
+class something():
+    def __init__(self, a,b):
+        self.a = a
+        self.b = b
+
+    
+    def equality(self, other):
+        return (self.a == other.a and self.b == other.b)
+
+
+
+# print(isinstance(1,int))
+# print(isinstance(1,(int,float)))
+# # A = something(1,2,3,4)
+# # print(isinstance(A,something))
+
+
+# for i in enumerate([1,2,3]):
+#     print(i)
+
+
+
+
+# class something():
+#     def __init__(self, a,b,c,d):
+#         self.a = a
+#         self.b = b
+#         self.c = c
+#         self.d = d
+    
+#     def __add__(self, other):
+#         return self.a+ other.a
+    
+#     def __sub__(self, other):
+#         return self.a- other.a
+
+        
+        
